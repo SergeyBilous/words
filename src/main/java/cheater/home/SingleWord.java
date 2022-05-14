262d037bc0209f94cb8556256f4759b43c9928ab
Sergey Bilous <bill-gh@yandex.ru>
2022-05-14 17:31:07 +0300

    Class for dictionary word created

---
+++ src/main/java/cheater/home/SingleWord.java
@@ -0,0 +1,44 @@
+package cheater.home;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleWord {
+    private String word;
+
+
+    public SingleWord(String word) {
+        this.word = word.toLowerCase();
+    }
+
+    public boolean compareWithTemplate(String template) {
+        if (template.length() != word.length()) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (template.charAt(i) == '_') {
+                continue;
+            }
+            if (template.charAt(i) == word.charAt(i)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkRestrictions(String letters) {
+
+    }
+
+    private static int numOfLetters(String word, char letter) {
+        int counter = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (letter == word.charAt(i)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/main/java/cheater/home/Starter.java
@@ -0,0 +1,7 @@
+package cheater.home;
+
+public class Starter {
+    public static void main(String[] args) {
+
+    }
+}
